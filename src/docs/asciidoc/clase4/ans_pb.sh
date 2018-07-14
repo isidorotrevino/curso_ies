5bcc0f73c3b41f4ac08b41b417b46c49ea12c9f6
Isidoro Treviño <isidoro.trevino@vintec.mx>
2018-07-14 12:47:16 -0500

    Avances

--- src/docs/asciidoc/clase4/ans_pb.sh
+++ src/docs/asciidoc/clase4/ans_pb.sh
@@ -3,8 +3,8 @@
 #export ANSIBLE_LIBRARY=/usr/local/lib/python2.7/dist-packages/ansible/modules/
 #export ANSIBLE_VAULT_PASSWORD_FILE=~/.gradle/ansible_pass.txt
 #ansible-playbook -e projectdir=$PWD --vault-password-file=$ANSIBLE_VAULT_PASSWORD_FILE $@
-export ANSIBLE_CONFIG=$PWD/config/ansible.cfg 
-export ANSIBLE_INVENTORY=$PWD/config/ansible_hosts
+export ANSIBLE_CONFIG=$PWD/ansible.cfg 
+export ANSIBLE_INVENTORY=$PWD/ansible_hosts
 export PROJECT_DIR=$PWD
 
 ansible-playbook -e projectdir=$PWD $@
